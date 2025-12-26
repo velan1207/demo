efad5cd0eb25d22ab9b9a1e8ed634f3b55287cb0
velan12072007 <velan12072007@gmail.com>
2025-12-26 13:12:30 +0530

    added leap yeasr logic

--- leap.py
+++ leap.py
@@ -1,5 +1,7 @@
 year=int(input("Enter year: "))
+print("Welcome to Leap Year Checker")
 if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
     print(f"{year} is a leap year.")
 else:
     print(f"{year} is not a leap year.")
+print("Thank you for using the Leap Year Checker!")
